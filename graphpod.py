837e65529d06eb2cdd44253780b72fb94b1c9147
SNEHA B PATIL <83591501+SnehaBPatil@users.noreply.github.com>
2024-03-25 11:25:00 +0530

    Update graphpod.py

--- graphpod.py
+++ graphpod.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-# Creating a graph
+
 import sqlite3
 
 conn = sqlite3.connect('collide.db')
@@ -22,15 +22,6 @@ graph.add_edge('P', 'Q', weight=900)
 graph.add_edge('R', 'S', weight=450)
 
 
-g.add_edge('A', 'B', weight=13)
-g.add_edge('B', 'Q', weight=20)
-g.add_edge('B', 'C', weight=7)
-g.add_edge('B', 'R', weight=15)
-g.add_edge('C', 'D', weight=17)
-g.add_edge('P', 'Q', weight=10)
-g.add_edge('R', 'S', weight=5)
-
-
 
 node_positions = {
     'A': (-12.55, 3.36),
@@ -43,23 +34,68 @@ node_positions = {
     'S': (-14.48, -7.88)
 }
 
-
-# nx.draw(graph, pos=node_positions, with_labels=True, node_size=1000, node_color='skyblue', font_size=12, font_weight='bold')
-# labels = nx.get_edge_attributes(graph, 'weight')
-# nx.draw_networkx_edge_labels(graph, pos=node_positions, edge_labels=labels)
-
 # Finding the shortest path using Dijkstra's algorithm
 def shortestPath(src, dest):
-    shortest_path = nx.dijkstra_path(graph, src, dest, weight='weight')
     shortest_distance = nx.dijkstra_path_length(graph, src, dest, weight='weight')
     return shortest_distance
 
+def collision(comming_to_stop,time,stop,id):
+    for c in comming_to_stop:
+        _, _, _, start_time, stop_time, _ = c
+        # stop time and start time are time at which the pod comes to stop and leaves the stop these are from table
+        if(id==22416):
+            print(f'with collision {c}')
+            print(f'time----{time}')
+        if ((abs(start_time - time) < 4)):
+            time=start_time+minimum_distance
+            if(id==22416):
+                print(f"Collision avoided {time}")
+    return time
 
-def short(src, dest):
-    shortest_path = nx.dijkstra_path(g, src, dest, weight='weight')
-    shortest_distance = nx.dijkstra_path_length(g, src, dest, weight='weight')
-    return shortest_distance
+def emptypod(stop,src,final_pod,present_start_time):
+    path=nx.dijkstra_path(graph, stop, src, weight='weight') #path from stop to source stop of passenger
+    id,_,_,_=final_pod
+    for i in range(0,len(path)-1):
 
+        cursor.execute("SELECT * FROM emptypod WHERE src = ?  and start_time>=? ORDER BY start_time",(path[i],present_start_time-8))
+        coming_to_stop=cursor.fetchall()
+        present_start_time=collision(coming_to_stop,present_start_time,path[i],1)
+
+
+
+
+        cursor.execute("SELECT * FROM journey WHERE src = ?  and start_time<=? ORDER BY start_time",(path[i],present_start_time-8))
+        coming_to_stop = cursor.fetchall()
+        present_start_time = collision(coming_to_stop, present_start_time, path[i],1)
+
+
+        #Next stop
+        present_stop_time=present_start_time+shortestPath(path[i], path[i+1])
+        i=i+1
+        # It selects row which has source as their present stop from table emptypod
+
+        cursor.execute("SELECT * FROM emptypod WHERE src = ?  and start_time>=? ORDER BY start_time", (path[i], present_stop_time-8))
+
+        coming_to_stop = cursor.fetchall()
+        present_stop_time = collision(coming_to_stop, present_stop_time, path[i],1)
+
+
+
+
+        # It selects row which has source as their present stop from table journey.
+
+        cursor.execute("SELECT * FROM journey WHERE src = ?  and start_time>=?",(path[i],present_stop_time-8))
+        coming_to_stop = cursor.fetchall()
+        present_stop_time = collision(coming_to_stop, present_stop_time, path[i],1)
+
+
+
+        cursor.execute("INSERT INTO emptypod (src, dest, start_time, stop_time, pod_id) VALUES (?, ?, ?, ?, ?)",
+                       (path[i-1], path[i], present_start_time, present_stop_time, id)) #update one part of journey in empytypod table.
+        conn.commit()
+        present_start_time = present_stop_time
+
+    return present_stop_time
 
 def pick(passenger_id, arrival_time, src, dest,path):
     min_time = 40000000
@@ -97,67 +133,81 @@ def pick(passenger_id, arrival_time, src, dest,path):
 
     travel_time = shortestPath(src, dest)
     if (stop != src):
-        distance = distance + short(stop, src)
+
+
         if (arrival_time > time):
-            pod_departure = arrival_time + weight
-            final_time = pod_departure + travel_time+board_deboard*2
+
+            present_start_time = emptypod(stop, src, final_pod,arrival_time)
+            # pod_departure = arrival_time + weight  #+weight
+            pod_departure=present_start_time
+            # final_time = pod_departure + travel_time
 
         else:
-            pod_departure = time + weight
-            final_time = pod_departure + travel_time+board_deboard*2
+            # pod_departure =  time+weight
+            present_start_time = emptypod(stop, src, final_pod,time)
+            pod_departure=present_start_time
+            # final_time = pod_departure + travel_time
     else:
         if (arrival_time > time):
             pod_departure = arrival_time
-            final_time = pod_departure + travel_time+board_deboard*2
+            # final_time = pod_departure + travel_time
         else:
             # print(f'pod_departure for {src} to {dest}')
             pod_departure = time
-            final_time = pod_departure + travel_time+board_deboard*2
-
-    l=[]
-    l.append(passenger_id)
-    for i in path:
-
-        # cursor.execute(
-        #     "SELECT * FROM passenger WHERE INSTR(path, ?) > 0 ",(i,))
-        query = """
-            SELECT * 
-            FROM passenger 
-            WHERE pid NOT IN ({}) 
-            AND INSTR(path, ?) > 0
-        """.format(', '.join(['?'] * len(l)))
+            # final_time = pod_departure + travel_time
 
 
-        cursor.execute(query, l + [i])
 
-        # Fetch the results
+
+    present_start_time=pod_departure
+    present_stop_time=0
+
+    for i in range(0,len(path)-1):
+
+
+        # It selects row which has source as their present stop from table emptypod
+        cursor.execute("SELECT * FROM emptypod WHERE src = ?  and start_time>=? ORDER BY start_time",(path[i],present_start_time-8))
 
         coming_to_stop = cursor.fetchall()
-        # print(len(coming_to_stop))
-        for c in coming_to_stop:
-
-            cid, a, dest, _, dep, _, pdep, _, _ = c
-            # print(f"pod departure of presenet {pod_departure}")
-            # print(f"pdep of presenet {pdep}")
-
-            if (dep!=0 and ((abs((pod_departure+shortestPath(src,i))-(pdep + shortestPath(a, i))) <4))):
-                print(i)
-                print(abs((pod_departure + shortestPath(src, i)) - (pdep + shortestPath(a, i))))
-                if(i==src):
-                    pod_departure = pod_departure + minimum_distance
-                final_time = final_time + minimum_distance
+        present_start_time = collision(coming_to_stop, present_start_time, path[i],passenger_id)
 
 
-                print(f'for passenger id{passenger_id}---{c}')
-                l.append(cid)
-    # print("next")
+
+
+        cursor.execute("SELECT * FROM journey WHERE src = ?  and start_time>=? ORDER BY start_time",(path[i],present_start_time-8))
+        # It selects row which has source as their present stop from table journey
+        coming_to_stop = cursor.fetchall()
+        present_start_time = collision(coming_to_stop, present_start_time, path[i],passenger_id)
+
+        if(i==0):
+            pod_departure=present_start_time
+        present_stop_time=present_start_time+shortestPath(path[i],path[i+1])
+        i=i+1   #checking next stop
+
+
+        cursor.execute("SELECT * FROM emptypod WHERE src = ?  and start_time>=? ORDER BY start_time",(path[i],present_stop_time-8))
+        coming_to_stop = cursor.fetchall()
+        present_stop_time = collision(coming_to_stop, present_stop_time, path[i],passenger_id)
+
+
+        cursor.execute("SELECT * FROM journey WHERE src = ?  and start_time<=? ORDER BY start_time",(path[i],present_stop_time-8))
+        coming_to_stop = cursor.fetchall()
+        present_stop_time = collision(coming_to_stop, present_stop_time, path[i],passenger_id)
+
+
+        cursor.execute("INSERT INTO journey (src, dest, start_time, stop_time, pid) VALUES (?, ?, ?, ?, ?)",
+                       (path[i - 1], path[i], present_start_time, present_stop_time, passenger_id))
+        conn.commit()  # update part of journey of passenger in journey table.
+
+        present_start_time=present_stop_time
+    final_time=present_stop_time
 
     cursor.execute(
         "UPDATE pod SET time = ?,current_stop=?,destination_stop=?  WHERE id = ?",
         (final_time, dest, dest, id))
     conn.commit()
     cursor.execute("UPDATE passenger set departure_time = ?,flag=?,pod_departure=?  WHERE pid = ?",
-                   (final_time, 1, pod_departure, passenger_id))
+                   (final_time, 1, pod_departure, passenger_id))  #update whole journey in passenger table.
     # print("Executed")
     conn.commit()
 
@@ -185,4 +235,3 @@ while (count != 0):
         passenger_id, cstop, dest, t, _, _, _, _, path = p
         pick(passenger_id, t, cstop, dest,path)
 
-# print(f"extra distance is {distance} km")
